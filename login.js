706d4170f424159bd95e248bdab82abee98fdc59
Suruj Priyam Hazarika <116090778+PriyamSuruj@users.noreply.github.com>
2024-10-01 15:25:21 +0530

    Create login.js

---
+++ login.js
@@ -0,0 +1,7 @@
+function toggleForms() {
+    var loginForm = document.getElementById('loginForm');
+    var signupForm = document.getElementById('signupForm');
+    
+    loginForm.classList.toggle('active');
+    signupForm.classList.toggle('active');
+  }
